b8d1650ed62e95c1ec7734cc1f49551f43ebec2e
Vinicius Cruz <viniciusrobertopro@gmail.com>
2025-09-17 23:34:31 -0300

    feat: add sanctum plugin

--- app/utils/env.ts
+++ app/utils/env.ts
@@ -2,7 +2,7 @@ import { z } from 'zod'
 
 const envSchema = z.object({
   NUXT_PUBLIC_API_BASE_URL: z.string().url(),
-  CONTAINER_API_BASE_URL: z.string().url(),
+  CONTAINER_API_BASE_URL: z.string().url().nullable(),
 })
 
 export const env = envSchema.parse(process.env)
--- app/utils/getApiBaseUrl.ts
+++ app/utils/getApiBaseUrl.ts
@@ -1,7 +1,10 @@
 import { env } from './env'
 
 export function getApiBaseUrl(): string {
+  const publicApiBaseUrl = env.NUXT_PUBLIC_API_BASE_URL
+  const containerApiBaseUrl = env.CONTAINER_API_BASE_URL
+
   return import.meta.client
-    ? env.NUXT_PUBLIC_API_BASE_URL
-    : env.CONTAINER_API_BASE_URL;
+    ? publicApiBaseUrl
+    : containerApiBaseUrl ?? publicApiBaseUrl;
 }
