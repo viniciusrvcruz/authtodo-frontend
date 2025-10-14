e656bf605c887a1c8798e146a5c7b0469e85174a
Vinicius Cruz <viniciusrobertopro@gmail.com>
2025-10-14 12:54:57 -0300

    feat(user-menu): update user menu to display user information

--- app/components/home/UserMenu.vue
+++ app/components/home/UserMenu.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
+import type { User } from '~/types/User.type';
+
 const { t } = useI18n()
+const { user } = useSanctumAuth<User>()
 
 const menu = ref();
 const items = ref([
@@ -10,7 +13,7 @@ const items = ref([
     label: t('components.home.user_menu.logout'),
     icon: 'pi pi-plus',
     command: () => {
-      console.log('teste')
+      console.log('logout')
     }
   },
 ]);
@@ -26,14 +29,14 @@ const toggle = (event: Event) => {
     <button class="flex items-start bg-gray-200 p-2 rounded-full cursor-pointer dark:bg-gray-500" @click="toggle">
       <Icon name="user" size="25" class="text-black" />
     </button>
-    <Menu ref="menu" :model="items" :popup="true" class="w-[200px]">
+    <Menu v-if="user" ref="menu" :model="items" :popup="true" class="w-[200px]">
       <template #start>
         <div class="p-2 w-full">
-          <h3 class="font-bold text-lg truncate" title="Nome do usuáriooooooooooooooooooooo">
-            Nome do usuáriooooooooooooooooooooo
+          <h3 class="font-bold text-lg truncate" :title="user?.name ?? ''">
+            {{ user.name }}
           </h3>
-          <p class="truncate" title="usuarioemail@gmail.comdddddddddddddd">
-            usuarioemail@gmail.comdddddddddddddd
+          <p class="truncate" :title="user.email">
+            {{ user.email }}
           </p>
         </div>
       </template>
--- app/types/schemas/User.schema.ts
+++ app/types/schemas/User.schema.ts
@@ -3,6 +3,7 @@ import { z } from 'zod'
 export const userSchema = z.object({
     id: z.string().uuid(),
     name: z.string({ required_error: 'required'}).nullable(),
+    email: z.string().email()
 })
 
 export const userNameSchema = z.object({
