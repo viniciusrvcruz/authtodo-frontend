9793d924d5d16785ecda831f150b31e399e2b4aa
Vinicius Cruz <viniciusrobertopro@gmail.com>
2025-10-25 16:41:55 -0300

    feat: create float input

---
+++ app/components/FloatInput.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import InputText from 'primevue/inputtext'
+import Textarea from 'primevue/textarea'
+
+const model = defineModel<string>()
+
+defineProps<{
+  type?: 'text' | 'textarea'
+  inputId: string
+  name?: string
+  placeholder: string
+  rows?: number
+}>()
+</script>
+
+<template>
+  <FloatLabel variant="on" class="w-full">
+    <component
+      :is="type === 'textarea' ? Textarea : InputText"
+      v-model="model"
+      class="w-full !rounded-xl"
+      :id="inputId"
+      :name="name"
+      :rows="rows"
+    />
+    <label :for="inputId" class="text-sm">
+      {{ placeholder }}
+    </label>
+  </FloatLabel>
+</template>
